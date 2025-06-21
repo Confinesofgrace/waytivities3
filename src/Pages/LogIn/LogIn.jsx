17529eb2233b4ce338f591be74c4ab65ab9cf845
Damilola Akinlolu <confinesofgrace@gmail.com>
2025-06-21 10:47:30 +0100

    Responsiveness- Login Page2

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -7,44 +7,49 @@ function LogIn () {
 
             <div id='login-modal'>
                 <div id='login-content'>
-                    <h3>Waytivities</h3>
-                    <p>Continue to your waytivities account</p>
 
-                    <hr style={{width:'100%', border:'1px solid', borderColor:'black', margin:'16px 0px'}}/>
+                    <div id='login-header'>
+                        <h3>Waytivities</h3>
+                        <p>Continue to your waytivities account</p>
+
+                        <hr/>
+
+                    </div>
+                    
 
                     
                     <div id='login-options'>
                     
-                    <div id='login-email'>
-                        Log In with Email
-                        
-                        <input id='login-input' placeholder='Email'/>
-                        <input id='login-input' placeholder='Password'/>
-                        <input id='login-input' placeholder='Confirm Password'/>
-                        <button
-                        style={{padding:'8px 16px',
-                            margin:'8px 0px',
-                            borderStyle:'none',
-                            border:'1px solid',
-                            borderRadius:'4px',
-                            backgroundColor:'black',
+                        <div id='login-email'>
+                            Log In with Email
                             
-                        }}
-                        ><Link style={{color:'white'}}>Log In</Link></button>
+                            <input id='login-input' placeholder='Email'/>
+                            <input id='login-input' placeholder='Password'/>
+                            <input id='login-input' placeholder='Confirm Password'/>
+                            <button
+                            style={{padding:'8px 16px',
+                                margin:'8px 0px',
+                                borderStyle:'none',
+                                border:'1px solid',
+                                borderRadius:'4px',
+                                backgroundColor:'black',
+                                
+                            }}
+                            ><Link style={{color:'white'}}>Log In</Link></button>
 
-                        <p style={{fontSize:'12px'}}>Forgot password?</p>
-                    </div>
+                            <p style={{fontSize:'10px'}}>Forgot password?</p>
+                        </div>
 
-                    <div id='or-divider'>
-                        <p>or</p>
-                    </div>
+                        <div id='or-divider'>
+                            <p>or</p>
+                        </div>
 
 
-                    <div id='login-alternative'>
-                        
-                        <div id='login-with'>Log In with Google</div>
-                        <div id='login-with'>Log In with Facebook</div>
-                    </div>
+                        <div id='login-alternative'>
+                            
+                            <div id='login-with'>Log In with Google</div>
+                            <div id='login-with'>Log In with Facebook</div>
+                        </div>
                     
 
                     
@@ -55,13 +60,17 @@ function LogIn () {
                     
                 </div>
 
-                <p>Don't have an account? <span><Link to= 'signup' 
+                <div id='new-user' >
+                    <p>Don't have an account? <span><Link to= 'signup' 
                     style={{
                         fontWeight:'600', 
                         color: 'purple', 
                         cursor:'pointer',
 
                     }}>Sign Up</Link></span></p>
+                </div>
+
+                
                 
             </div>
         
