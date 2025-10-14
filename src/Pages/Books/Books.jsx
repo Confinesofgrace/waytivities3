74bc69dbc643b06a5d54afa76b25a9913249c368
Damilola Akinlolu <confinesofgrace@gmail.com>
2025-10-14 04:53:54 +0100

    BookPreview component added to Books page

--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,10 +1,10 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { db } from '../../firebaseConfig';
-import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 import './Books.css';
 import BookListsPreview from "../../Components/BookListsPreview";
-
+import BookPreview from '../../Components/BookPreview';
 import Footer from '../../Components/Footer';
 
 function Books() {
@@ -20,24 +20,19 @@ function Books() {
             const querySnapshot = await getDocs(collection(db, 'books'));
             const booksArray = querySnapshot.docs.map(docSnap => ({
                 id: docSnap.id,
-                ...docSnap.data()
+                ...docSnap.data(),
             }));
             setBooks(booksArray);
 
+            // If a bookId is provided, find it in the fetched array
             if (bookId) {
                 const foundBook = booksArray.find(b => b.id === bookId);
-                if (foundBook) {
-                    setSelectedBook(foundBook);
-                } else {
-                    const bookRef = doc(db, 'books', bookId);
-                    const bookSnap = await getDoc(bookRef);
-                    if (bookSnap.exists()) {
-                        setSelectedBook({ id: bookSnap.id, ...bookSnap.data() });
-                    }
-                }
-            } else if (booksArray.length > 0) {
+                setSelectedBook(foundBook || booksArray[0]);
+            } else {
+                // Default: first book
                 setSelectedBook(booksArray[0]);
             }
+
             setLoading(false);
         };
 
@@ -46,65 +41,69 @@ function Books() {
 
     const handleBookClick = (book) => {
         setSelectedBook(book);
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
     };
 
     const toggleExpand = () => {
         setIsExpanded(!isExpanded);
     };
 
+
+
     return (
         <span>
-
-        <div id='page-layout'>
-            <div className="relative-container">
-                <div id='books-layout'>
-                    {loading ? (
-                        <p className="loading-message">Loading books...</p>
-                    ) : selectedBook ? (
-                        <div id='preview-box' className="preview-flex" key={selectedBook.id}>
-                            <div id='book-preview'>
-                                <div
-                                    id='book-cover'
-                                    className={selectedBook.frontCover ? "with-cover" : "no-cover"}
-                                    style={{
-                                        backgroundImage: selectedBook.frontCover ? `url(${selectedBook.frontCover})` : "none"
-                                    }}
-                                >
-                                    {!selectedBook.frontCover && <p>{selectedBook.title}</p>}
+            <div id='page-layout'>
+                <div className="relative-container">
+                    <div id='books-layout'>
+                        {loading ? (
+                            <p className="loading-message">Loading books...</p>
+                        ) : selectedBook ? (
+                            <div id='preview-box' className="preview-flex" key={selectedBook.id}>
+                                <div id='book-preview'>
+                                    <div
+                                        id='book-cover'
+                                        className={selectedBook.frontCover ? "with-cover" : "no-cover"}
+                                        style={{
+                                            backgroundImage: selectedBook.frontCover
+                                                ? `url(${selectedBook.frontCover})`
+                                                : "none",
+                                        }}
+                                    >
+                                        {!selectedBook.frontCover && <p>{selectedBook.title}</p>}
+                                    </div>
+                                    <div
+                                        id='description-overlay'
+                                        className={isExpanded ? 'expanded' : ''}
+                                        onClick={toggleExpand}
+                                    >
+                                        <p>
+                                            <span className="book-title">{selectedBook.title}</span>
+                                            <br />
+                                            {selectedBook.aboutBook}
+                                        </p>
+                                        <div id='fade' className={isExpanded ? 'hidden' : ''}></div>
+                                    </div>
                                 </div>
-                                <div
-                                    id='description-overlay'
-                                    className={isExpanded ? 'expanded' : ''}
-                                    onClick={toggleExpand}
-                                >
-                                    <p>
-                                        <span className="book-title">{selectedBook.title}</span>
-                                        <br />
-                                        {selectedBook.aboutBook}
-                                    </p>
-                                    <div id='fade' className={isExpanded ? 'hidden' : ''}></div>
+                                <div id='book-trailer-container'>
+                                    <div id='book-trailer'>
+                                        <p>{selectedBook.trailer}</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div id='book-trailer-container'>
-                                <div id='book-trailer'>
-                                    <p>{selectedBook.trailer}</p>
-                                </div>
-                            </div>
-                        </div>
-                    ) : (
-                        <p className="empty-message">No books available</p>
-                    )}
-                </div>
-                <div id='booklists'>
-                   <BookListsPreview books={books} onBookClick={handleBookClick} />
+                        ) : (
+                            <p className="empty-message">No books available</p>
+                        )}
+                    </div>
 
+                    <div id='booklists'>
+                        <BookPreview />
+                    </div>
                 </div>
-
             </div>
-
-        </div>
-        
-        <Footer/>
+            <Footer />
         </span>
     );
 }
