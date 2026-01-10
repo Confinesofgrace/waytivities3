a334656a0d436e0f8e5dcd14563259bf75a0ac14
Damilola Akinlolu <confinesofgrace@gmail.com>
2026-01-10 21:13:24 +0100

    About us layout

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,8 +2,25 @@ import './About.css'
 
 function About () {
     return (
-        <div id='page-layout'>
-            This is the About us page.
+        <div id='aboutus-page'>
+            <div id='aboutus-layout'>
+                <div id ='aboutus-title'>
+                        <p>ABOUT US</p>
+                </div> 
+                <div id='aboutus-left'> </div>
+                <div id='aboutus-right'>
+                    
+                    
+                    <div id='aboutus'>
+                        <p>This is the About us page.</p>
+                        <p>Waytivities is a platform dedicated to connecting individuals through shared activities and interests. Our mission is to foster meaningful connections and create memorable experiences by bringing people together in a fun and engaging way. Whether you're looking to join a local hiking group, find a cooking class, or participate in community events, Waytivities is here to help you discover new opportunities and make lasting friendships. Join us today and start exploring the world of activities waiting for you!</p>
+                    
+                    </div> 
+
+                </div>
+                
+            </div>
+            
         </div>
     );
 }
